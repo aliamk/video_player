2c822a6f4657cffec93b61d2db4f4527c6650f53
aliamk <alia.azarian@gmail.com>
2020-07-19 23:00:35 +0100

    added function for calculating the video\'s elapsed time as a percentage

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const playBtn = document.getElementById('play-button')
 const volumeIcon = document.getElementById('volume-icon')
 const volumeRange = document.querySelector('.volume-range')
 const volumeBar = document.querySelector('.volume-bar')
-const currentTime = document.querySelector('.time-elapse')
+const currentTime = document.querySelector('.time-elapsed')
 const duration = document.querySelector('.time-duration')
 const fullscreenBtn = document.querySelector('.fullscreen')
 
@@ -33,7 +33,10 @@ function togglePlay() {
 video.addEventListener('ended', showPlayIcon)
 
 // Progress Bar ---------------------------------- //
-
+function updateProgress() {
+  // console.log('currentTime', video.currentTime, 'duration', video.duration)
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100 }%`
+}
 
 
 // Volume Controls --------------------------- //
@@ -49,3 +52,5 @@ video.addEventListener('ended', showPlayIcon)
 // Event Listeners
 playBtn.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
+video.addEventListener('timeupdate', updateProgress)
+video.addEventListener('canPlay', updateProgress)
